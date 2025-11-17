e2385d109c37d7d2abd984a6d18aea52dd28b3a0
TifanyLopez <144163025+TifanyLopez@users.noreply.github.com>
2025-11-17 14:52:23 -0600

    actualizar

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\HP\Desktop\Vista\venv'
+set -gx VIRTUAL_ENV 'C:\Users\HP\Documents\GitHub\Vista\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
